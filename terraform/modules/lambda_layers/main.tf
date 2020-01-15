8c93440583e32b6762e1fa89c0b483427a40ccaa
jterry64 <justin.terry@wri.org>
2020-01-15 17:26:16 -0500

    Fixing RasterIO layer and removing pandas layer

--- terraform/modules/lambda_layers/main.tf
+++ terraform/modules/lambda_layers/main.tf
@@ -1,8 +1,15 @@
-resource "aws_s3_bucket_object" "shapely" {
+resource "aws_s3_bucket_object" "shapely_pyyaml" {
   bucket = var.s3_bucket
-  key    = "lambda_layers/shapely.zip"
-  source = "../docker/shapely/layer.zip"
-  etag   = filemd5("../docker/shapely/layer.zip")
+  key    = "lambda_layers/shapely_pyyaml.zip"
+  source = "../docker/shapely_pyyaml/layer.zip"
+  etag   = filemd5("../docker/shapely_pyyaml/layer.zip")
+}
+
+resource "aws_lambda_layer_version" "shapely_pyyaml" {
+  layer_name          = substr("${var.project}-shapely_pyyaml", 0, 64)
+  s3_bucket           = aws_s3_bucket_object.shapely_pyyaml.bucket
+  s3_key              = aws_s3_bucket_object.shapely_pyyaml.key
+  compatible_runtimes = ["python3.7"]
 }
 
 resource "aws_s3_bucket_object" "rasterio" {
@@ -12,31 +19,9 @@ resource "aws_s3_bucket_object" "rasterio" {
   etag   = filemd5("../docker/rasterio/layer.zip")
 }
 
-resource "aws_s3_bucket_object" "pandas" {
-  bucket = var.s3_bucket
-  key    = "lambda_layers/pandas.zip"
-  source = "../docker/pandas/layer.zip"
-  etag   = filemd5("../docker/pandas/layer.zip")
-}
-
-resource "aws_lambda_layer_version" "shapely" {
-  layer_name          = substr("${var.project}-shapely", 0, 64)
-  s3_bucket           = aws_s3_bucket_object.shapely.bucket
-  s3_key              = aws_s3_bucket_object.shapely.key
-  compatible_runtimes = ["python3.6", "python3.7"]
-}
-
 resource "aws_lambda_layer_version" "rasterio" {
   layer_name          = substr("${var.project}-rasterio", 0, 64)
   s3_bucket           = aws_s3_bucket_object.rasterio.bucket
   s3_key              = aws_s3_bucket_object.rasterio.key
   compatible_runtimes = ["python3.6"]
 }
-
-resource "aws_lambda_layer_version" "pandas" {
-  layer_name          = substr("${var.project}-pandas", 0, 64)
-  s3_bucket           = aws_s3_bucket_object.pandas.bucket
-  s3_key              = aws_s3_bucket_object.pandas.key
-  compatible_runtimes = ["python3.6", "python3.7"]
-}
-

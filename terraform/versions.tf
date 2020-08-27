6141f807d1bb6bc3cb29812b34809ec96101831c
Thomas Maschler <tmaschler@wri.org>
2020-08-27 17:43:31 -0400

    update to Terraform 0.13

---
+++ terraform/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 2.56.0, < 4.0"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
+  required_version = ">= 0.13"
+}

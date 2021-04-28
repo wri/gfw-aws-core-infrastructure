c291336caa0b469a5372742eed1c8653987e6f10
Thomas Maschler <tmaschler@wri.org>
2021-04-28 11:55:22 -0400

    add Job tag to resources

--- terraform/modules/sns/main.tf
+++ terraform/modules/sns/main.tf
@@ -11,17 +11,17 @@ data "aws_iam_policy_document" "partner_sns_publish_policy_doc" {
   statement {
     actions   = ["sns:Publish"]
     resources = [aws_sns_topic.data_discovery_topic.arn]
-    effect = "Allow"
+    effect    = "Allow"
   }
 }
 
- resource "aws_iam_policy" "policy" {
-   name        = "partner_sns_publish_policy"
-   description = "Policy for partners/services to publish data update availability to"
-   policy = data.aws_iam_policy_document.partner_sns_publish_policy_doc.json
+resource "aws_iam_policy" "policy" {
+  name        = "partner_sns_publish_policy"
+  description = "Policy for partners/services to publish data update availability to"
+  policy      = data.aws_iam_policy_document.partner_sns_publish_policy_doc.json
 }
 
 resource "aws_iam_group_policy_attachment" "attachment" {
   policy_arn = aws_iam_policy.policy.arn
-  group = aws_iam_group.sns_discovery_publishers_group.name
+  group      = aws_iam_group.sns_discovery_publishers_group.name
 }

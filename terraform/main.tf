394fea20a39fced8f76a5f84817c03b0c75261b3
Daniel Mannarino <daniel.mannarino@gmail.com>
2026-02-23 12:07:54 -0500

    Staging -> production merge up of deploy fixes, parameter store additions (#135)
    * Add values to parameter store needed for Data API (#131)
    * Use new TF Docker image to fix deploy (#133)

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,7 +68,7 @@ module "sns" {
 }
 
 module "data-lake_bucket" {
-  source         = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/storage?ref=v0.4.2.11"
+  source         = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/storage?ref=v0.4.2.13"
   bucket_name    = "gfw-data-lake${local.bucket_suffix}"
   project        = var.project_prefix
   requester_pays = true
@@ -99,7 +99,7 @@ module "data-lake_bucket" {
 }
 
 module "pipeline_bucket" {
-  source         = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/storage?ref=v0.4.2.11"
+  source         = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/storage?ref=v0.4.2.13"
   bucket_name    = "gfw-pipelines${local.bucket_suffix}"
   project        = var.project_prefix
   requester_pays = false
@@ -141,7 +141,7 @@ module "pipeline_bucket" {
 
 module "data-lake-test-bucket" {
   count          = var.environment == "dev" ? 1 : 0
-  source         = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/storage?ref=v0.4.2.11"
+  source         = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/storage?ref=v0.4.2.13"
   bucket_name    = "gfw-data-lake-test"
   requester_pays = true
   project        = var.project_prefix
@@ -151,7 +151,7 @@ module "data-lake-test-bucket" {
 
 module "pipeline-test-bucket" {
   count          = var.environment == "dev" ? 1 : 0
-  source         = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/storage?ref=v0.4.2.11"
+  source         = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/storage?ref=v0.4.2.13"
   bucket_name    = "gfw-pipelines-test"
   requester_pays = false
   project        = var.project_prefix
@@ -171,28 +171,28 @@ module "firewall" {
 }
 
 module "api_token_secret" {
-  source        = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/secrets?ref=v0.4.2.11"
+  source        = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/secrets?ref=v0.4.2.13"
   project       = var.project_prefix
   name          = "gfw-api/token"
   secret_string = jsonencode({ "token" = var.gfw_api_token, "email" = "gfw-sync@wri.org" })
 }
 
 module "slack_secret" {
-  source        = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/secrets?ref=v0.4.2.11"
+  source        = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/secrets?ref=v0.4.2.13"
   project       = var.project_prefix
   name          = "slack/gfw-sync"
   secret_string = jsonencode({ "data-updates" = var.slack_data_updates_hook })
 }
 
 module "gcs_gfw_gee_export_secret" {
-  source        = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/secrets?ref=v0.4.2.11"
+  source        = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/secrets?ref=v0.4.2.13"
   project       = var.project_prefix
   name          = "gcs/gfw-gee-export"
   secret_string = var.gfw-gee-export_key
 }
 
 module "planet_api_key_secret" {
-  source        = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/secrets?ref=v0.4.2.11"
+  source        = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/secrets?ref=v0.4.2.13"
   project       = var.project_prefix
   name          = "planet/api_key"
   secret_string = var.planet_api_key
@@ -234,24 +234,33 @@ module "redis" {
 }
 
 module "ssm" {
-  source      = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/ssm?ref=v0.4.2.11"
+  source      = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/ssm?ref=v0.4.2.13"
   environment = var.environment
   namespace   = "gfw-aws-core-infra"
   contract = {
-    acm_certificate_arn                = aws_acm_certificate.globalforestwatch_new[0].arn
-    data_lake_bucket_name              = module.data-lake_bucket.bucket_id
-    gfw_pipelines_bucket_name          = module.pipeline_bucket.bucket_id
-    gfw_data_api_token_arn             = module.api_token_secret.secret_arn
-    gfw_data_api_token_read_policy_arn = module.api_token_secret.read_policy_arn
-    planet_secret_arn                  = module.planet_api_key_secret.secret_arn
-    planet_secret_policy_arn           = module.planet_api_key_secret.read_policy_arn
-    postgresql_reader_secret_arn       = module.postgresql.secrets_postgresql-reader_arn
+    acm_certificate_arn                 = aws_acm_certificate.globalforestwatch_new[0].arn
+    aurora_cluster_instance_class       = module.postgresql.aurora_cluster_instance_class
+    data_lake_bucket_name               = module.data-lake_bucket.bucket_id
+    default_security_group_id           = module.firewall.default_security_group_id
+    gfw_gee_export_read_policy_arn      = module.gcs_gfw_gee_export_secret.read_policy_arn
+    gfw_gee_export_secret_arn           = module.gcs_gfw_gee_export_secret.secret_arn
+    gfw_pipelines_bucket_name           = module.pipeline_bucket.bucket_id
+    gfw_data_api_token_arn              = module.api_token_secret.secret_arn
+    gfw_data_api_token_read_policy_arn  = module.api_token_secret.read_policy_arn
+    iam_policy_s3_write_data_lake_arn   = module.data-lake_bucket.write_policy_arns[0]
+    planet_secret_arn                   = module.planet_api_key_secret.secret_arn
+    planet_secret_policy_arn            = module.planet_api_key_secret.read_policy_arn
+    postgresql_reader_policy_arn        = module.postgresql.secrets_postgresql-reader_policy_arn
+    postgresql_reader_secret_arn        = module.postgresql.secrets_postgresql-reader_arn
     postgresql_reader_secret_policy_arn = module.postgresql.secrets_postgresql-reader_policy_arn
-    postgresql_security_group_id       = module.postgresql.security_group_id
-    private_subnet_ids                 = module.vpc.private_subnet_ids
-    public_subnet_ids                  = module.vpc.public_subnet_ids
-    tags                               = local.tags
-    vpc_id                             = module.vpc.id
+    postgresql_security_group_id        = module.postgresql.security_group_id
+    postgresql_writer_policy_arn        = module.postgresql.secrets_postgresql-writer_policy_arn
+    postgresql_writer_secret_arn        = module.postgresql.secrets_postgresql-writer_arn
+    postgresql_security_group_id        = module.postgresql.security_group_id
+    private_subnet_ids                  = module.vpc.private_subnet_ids
+    public_subnet_ids                   = module.vpc.public_subnet_ids
+    tags                                = local.tags
+    vpc_id                              = module.vpc.id
   }
   lists = {}
   strings = {}
